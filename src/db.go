24cd461cecd05750282be9a491a1354357ee6f4e
stan <stan_wong@foxmail.com>
2023-10-18 18:40:45 +0800

    ...

--- src/db.go
+++ src/db.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"log"
-
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var db *gorm.DB
 
 func init() {
-	dsn := "stan:j@tcp(127.0.0.1:3306)/blog?charset=utf8mb4&parseTime=True&loc=Local"
+	/* dsn := "stan:j@tcp(127.0.0.1:3306)/blog?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("fail to init db")
-	}
+	} */
 }
